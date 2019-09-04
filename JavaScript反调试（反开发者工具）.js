1bedbeb85092937a1b0ef4127e4c5dfd7a2a3af5
青绾 <38388662+yangx14488@users.noreply.github.com>
2019-09-04 15:45:43 +0800

    Update JavaScript反调试（反开发者工具）.js

--- JavaScript反调试（反开发者工具）.js
+++ JavaScript反调试（反开发者工具）.js
@@ -27,54 +27,58 @@ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR I
   *
   * 所有的函数可能都会多次触发，因为谁也不知道破解者到底会开启多少次控制台
   */
-anti_developer_tools = mod => { // 记得改成 module.exports
-  let a, b, c, d, e, f, g, h ;
-  let i, j, k, l, m, n, o, p, q ;
-  a = 5 ;
-  p = console.log ;
-  q = console.clear || false;
-  b = "Your text here" ;
-  c = ( ) => new Date ;
-  h = ( ) => { while ( g-->a>>6 ) ( ( ( ) => { } ) [ "constructor" ]( `/*\n${b}\n*/\ndebugger;` )( ) ) } ;
-  e = ( z = false ) => ( ( d = z ) || ( f = c( g = a ) , h( ) , e( c( ) - f < a+1 ) ) ) ;
-  i = ( a = true ) => j = a ;
-  if ( !!~navigator.userAgent.toLocaleLowerCase( ).indexOf( "firefox" ) ) {
-  k = /./ ;
-  k.toString = i ;
-  } else {
-  k = new Image( ) ;
-  k.__defineGetter__( "id", i ) ;
-  } ;
-  l = ( a, b ) => {
-    typeof mod === "object" && typeof mod[ a ] === "function" && mod[ a ]( ) ;
-    n = b ;
-  } ;
-  n = 1 ;
-  o = ( ) => {
-    i( false ) ;
-    try {
-      p( k ) ;
-      q && q( ) ;
-      p( b ) ;
-    } catch ( e ) {
-      i( ) ;
+  ( function( ){
+    function anti_developer_tools ( mod ) {
+      var a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q ;
+      a = 5 ;
+      p = console.log ;
+      q = console.clear || false;
+      b = "by 青绾( yangx14488@foxmail.com )\n试试吧，解锁是 unlock( ) 哦" ;
+      c = function ( ) { return new Date } ;
+      h = function ( ) { while ( g-->a>>6 ) ( function( ){ } )[ "constructor" ]( "/*\n"+b+"\n*/\ndebugger;" )( ) } ;
+      e = function ( z ) { if ( typeof z !== "boolean" ) z = false ; return ( ( d = z ) || ( f = c( g = a ) , h( ) , e( c( ) - f < a+1 ) ) ) } ;
+      i = function ( a ) { if ( typeof a !== "boolean" ) a = true ; return j = a } ;
+      if ( !!~navigator.userAgent.toLocaleLowerCase( ).indexOf( "firefox" ) ) {
+        k = /./ ;
+        k.toString = i ;
+      } else {
+        k = new Image( ) ;
+        k.__defineGetter__( "id", i ) ;
+      } ;
+      l = function ( a, b ) {
+        typeof mod === "object" && typeof mod[ a ] === "function" && mod[ a ]( ) ;
+        n = b ;
+      } ;
+      n = 1 ;
+      o = function ( ) {
+        i( false ) ;
+        try {
+          p( k ) ;
+          q && q( ) ;
+          p( b ) ;
+        } catch ( e ) {
+          i( ) ;
+        } ;
+      } ;
+      m = function ( ) {
+        o( ) ; // 检测控制台的开启状况
+        j && n !== 0 && n!== 2 && l( "n", 0 ) ; // 判断是否需要输出
+        e( ) ; // 检查断点
+        o( ) ; // 再次检测控制台的开启状况
+        j ? ( d && n!==2 && l( "warn", 2 ) ) : ( n!==1 && l( "f", 1 ) ) ;
+      } ;
+      setInterval( m, a<<6 ) ;
+      m( ) ;
     } ;
-  } ;
-  m = ( ) => {
-    if ( d ) {
-      o( ) ;
-      j && n !== 0 && n!== 2 && l( "n", 0 ) ;
-      e( ) ;
-      o( ) ;
-      j ? d && n!==2 && l( "warn", 2 ) : n!==1 && l( "f", 1 ) ;
-    } ;
-  } ;
-  setInterval( m, a<<6 ) ;
-  e( ) ;
-} ;
-
-anti_developer_tools ( {
-  n : ( )=>{ alert( "打开了控制台" ) } ,
-  f : ( )=>{ alert( "关闭了控制台" ) } ,
-  warn : ( )=>{ alert( "停用了断点" ) }
-} ) ;
+    anti_developer_tools ( {
+      n : function ( ) {
+        // 用户打开了开发者模式时，会调用这个函数
+      } ,
+      f : function ( ) {
+        // 用户关闭了开发者模式时，会调用这个函数
+      } ,
+      warn : function ( ) {
+        // 如果用户尝试强制破解时，或者是IE11及以下（迷），会调用这个函数
+      }
+    } ) ;
+  } )( ) ;
